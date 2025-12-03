5cdbf4a5c51e24b6be6a08d437ed43fada7fae8a
Aqil Aswangga <aqilaswangga1@gmail.com>
2025-12-03 07:07:00 +0000

    di background sudah bagus berbentuk grid, coba tambahkan animasi gelomba

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,7 +1,9 @@
 export function Background() {
   return (
     <div
-      className="fixed inset-0 -z-10 h-full w-full bg-background bg-grid"
-    ></div>
+      className="fixed inset-0 -z-10 h-full w-full bg-background"
+    >
+      <div className="absolute inset-0 h-full w-full bg-grid"></div>
+    </div>
   );
 }
