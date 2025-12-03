269fe178633cda0df4adf10cbf6a5807426eb4e2
Aqil Aswangga <aqilaswangga1@gmail.com>
2025-12-03 06:41:05 +0000

    set the background color to be just 1 not belang2

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,9 +1,7 @@
 export function Background() {
   return (
     <div
-      className="fixed inset-0 -z-10 h-full w-full"
-    >
-      <div className="absolute inset-0 bg-background"></div>
-    </div>
+      className="fixed inset-0 -z-10 h-full w-full bg-background"
+    ></div>
   );
 }
