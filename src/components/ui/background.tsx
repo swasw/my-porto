318da30ee0f96be91863cc971aa2137235bd9fbf
Aqil Aswangga <aqilaswangga1@gmail.com>
2025-12-03 06:37:16 +0000

    the background image is not  in still posisition, when we slide down , i

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,7 +1,7 @@
 export function Background() {
   return (
     <div
-      className="fixed inset-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat bg-fixed opacity-20"
+      className="fixed inset-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-20"
       style={{
         backgroundImage:
           "url('https://picsum.photos/seed/tech/1920/3000')",
