c3ef5531c503e5c02fce63bc4a85898dc7c116d9
Aqil Aswangga <aqilaswangga1@gmail.com>
2025-12-03 06:46:30 +0000

    add computer ish motives to the sliding background

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,7 +1,7 @@
 export function Background() {
   return (
     <div
-      className="fixed inset-0 -z-10 h-full w-full bg-background"
+      className="fixed inset-0 -z-10 h-full w-full bg-background bg-grid"
     ></div>
   );
 }
