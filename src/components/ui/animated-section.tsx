d9de20c6e6beb1b9a7739e33a122646f1593997b
Aqil Aswangga <aqilaswangga1@gmail.com>
2025-12-03 06:17:53 +0000

    animasinya ganti slide from left dong

--- src/components/ui/animated-section.tsx
+++ src/components/ui/animated-section.tsx
@@ -44,7 +44,7 @@ export function AnimatedSection({
       ref={ref}
       className={cn(
         'transition-all duration-700 ease-in-out',
-        isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10',
+        isVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10',
         className
       )}
     >
